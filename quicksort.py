73a16ac1525fea117f6ab48242b8d78550a21fed
marcusraitner <65543240+marcusraitner@users.noreply.github.com>
2020-05-18 13:43:30 +0200

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,26 @@
+import numpy.random as npr
+
+
+def quicksort(input_list):
+    if len(input_list) <= 1:
+        return input_list
+    else:
+        # get random pivot element
+        length = len(input_list)
+        rnd = npr.randint(0, length-1)
+        pivot = input_list[rnd]
+        lower = []
+        higher = []
+        for i in input_list:
+            if i < pivot:
+                lower.append(i)
+            elif i > pivot:
+                higher.append(i)
+        # print(lower, "//", higher)
+        return quicksort(lower) + [pivot] + quicksort(higher)
+
+
+if __name__ == '__main__':
+    unsorted = npr.randint(0, 100000, 100)
+    print(unsorted)
+    print(quicksort(unsorted))
